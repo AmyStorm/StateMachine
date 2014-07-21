55da3ed64289e79b3198ae5be234f925437f339d
AmyStorm <khaerothe@gmail.com>
2014-07-21 14:26:50 +0800

    Initial commit

---
+++ src/main/java/com/zzy/StateMachine.java
@@ -0,0 +1,14 @@
+package com.zzy;
+
+/**
+ * Hello world!
+ *
+ */
+public class StateMachine
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+}
